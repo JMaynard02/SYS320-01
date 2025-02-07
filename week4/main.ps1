96b5e18d26f08ee043b651675eb70253682144d9
JMaynard02 <justin.maynard@mymail.champlain.edu>
2025-02-07 18:18:00 -0500

    Update main.ps1 for Parsing Apache Logs
    Updating to work with second assignment

--- week4/main.ps1
+++ week4/main.ps1
@@ -1,6 +1,12 @@
 . (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot Parsing-Apache-Logs.ps1)
 
 clear
 
+#Windows Apache Logs Assignment
 $ipsTable = GetIPAddress -page 'index.html' -code ' 200 ' -browser 'Chrome'
 $ipsTable
+
+#Parsing Apache Logs Assignment
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
