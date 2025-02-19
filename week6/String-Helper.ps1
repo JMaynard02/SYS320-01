b60621039bd684ce79c0c90d757f1f75c9a9fef0
JMaynard02 <justin.maynard@mymail.champlain.edu>
2025-02-19 14:23:48 -0500

    Add files via upload

---
+++ week6/String-Helper.ps1
@@ -0,0 +1,44 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+function checkPassword($password) {
+    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringtoBSTR($password)
+    $plainpassword = [System.Runtime.InteropServices.Marshal]::PTRtoStringAuto($bstr)
+
+    if ($plainpassword -notmatch "[^a-zA-Z0-9\s]" -or $plainpassword -notmatch "\d" -or `
+        $plainpassword -notmatch "[a-zA-Z]") {
+        return $false
+    }
+    if ($plainpassword.Length -lt 6) {
+        return $false
+    }
+
+    return $true
+}
---
+++ week6/main.ps1
@@ -0,0 +1,165 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+
+clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - List Enabled Users`n"
+$Prompt += "2 - List Disabled Users`n"
+$Prompt += "3 - Create a User`n"
+$Prompt += "4 - Remove a User`n"
+$Prompt += "5 - Enable a User`n"
+$Prompt += "6 - Disable a User`n"
+$Prompt += "7 - Get Log-In Logs`n"
+$Prompt += "8 - Get Failed Log-In Logs`n"
+$Prompt += "9 - List At-Risk Users`n"
+$Prompt += "10 - Exit`n"
+
+
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 10){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -eq 1){
+        $enabledUsers = getEnabledUsers
+        Write-Host ($enabledUsers | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 2){
+        $notEnabledUsers = getNotEnabledUsers
+        Write-Host ($notEnabledUsers | Format-Table | Out-String)
+    }
+
+
+    # Create a user
+    elseif($choice -eq 3){ 
+
+        $name = Read-Host -Prompt "Please enter the username for the new user"
+        if (checkUser($name)) {
+            Write-Host "User already exists" | Out-String
+        }
+        else {
+            $password = Read-Host -AsSecureString -Prompt "Please enter the password for the new user"
+            if (checkPassword($password)) {
+
+                createAUser $name $password
+
+                Write-Host "User: $name is created." | Out-String
+            }
+            else {
+                Write-Host "Password must be at least 6 characters and contain at least 1 special character, 1 number, and 1 letter" | Out-String   
+            }
+        }
+    }
+
+
+    # Remove a user
+    elseif($choice -eq 4){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be removed"
+
+        if (checkUser ($name)) {
+            removeAUser $name
+
+            Write-Host "User: $name Removed." | Out-String
+        }
+        else {
+            Write-Host "User not found" | Out-String
+        }
+    }
+
+
+    # Enable a user
+    elseif($choice -eq 5){
+
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be enabled"
+
+        if (checkUser ($name)) {
+            enableAUser $name
+
+            Write-Host "User: $name Enabled." | Out-String
+        }
+        else {
+            Write-Host "User not found" | Out-String
+        }
+    }
+
+
+    # Disable a user
+    elseif($choice -eq 6){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be disabled"
+
+        if (checkUser ($name)) {
+            disableAUser $name
+
+            Write-Host "User: $name Disabled." | Out-String
+        }
+
+        else {
+            Write-Host "User not found" | Out-String
+        }
+    }
+
+
+    elseif($choice -eq 7){
+
+        $name = Read-Host -Prompt "Please enter the username for the user logs"
+
+        if (checkUser ($name)) {
+            $days = Read-Host -Prompt "Please enter number of days of logs to look at"
+            $userLogins = getLogInAndOffs $days
+            
+
+            Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+        }
+        else {
+            Write-Host "User not found" | Out-String
+        }
+    }
+
+
+    elseif($choice -eq 8){
+
+        $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
+
+        if (checkUser ($name)) {
+            $days = Read-Host -Prompt "Please enter number of days of logs to look at"
+            $userLogins = getFailedLogins $days
+
+            Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+        }
+        else {
+            Write-Host "User not found" | Out-String
+        }
+    }
+    
+    # At-Risk Users
+    elseif($choice -eq 9){
+        $days = Read-Host -Prompt "Please enter number of days of logs to look at"
+        $userLogins = getFailedLogins $days
+        Write-Host "Displaying users with 10 or more failed login attempts" | Out-String
+
+        Write-Host ($userLogins | Group-Object -Property User | Where-Object Count -ge 10 |`
+        Select-Object Name, Count | Format-Table | Out-String)
+    }
+
+    # Validation
+    else {
+        Write-Host "Please enter a valid number corresponding with one of the choices (from 1-10)"
+    }
+}
